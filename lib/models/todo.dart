cdfb1f6ab0f2d6087b0b4da8e7e5c136646552bb
Raman Tank <ramantank04022002@gmail.com>
2023-02-23 11:51:32 +0530

    Updation in application

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -9,11 +9,6 @@ class ToDo {
   static List<ToDo> todoList() {
     return [
       ToDo(id: '01', todoText: 'Coding',isDone: true),
-      ToDo(id: '02', todoText: 'Coding1',isDone: true),
-      ToDo(id: '03', todoText: 'Coding2',),
-      ToDo(id: '04', todoText: 'Coding3',isDone: false),
-      ToDo(id: '05', todoText: 'Coding4',),
-      ToDo(id: '06', todoText: 'Coding5',isDone: true),
-    ];
+      ];
   }
 }
